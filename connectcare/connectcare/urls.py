e74230d5d7a0e0e48d82c5abdadd82bec32beb2e
123tarunanand <anandtarun2@gmail.com>
2018-03-03 11:11:20 +0530

    Registration

--- connectcare/connectcare/urls.py
+++ connectcare/connectcare/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-
+from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^',include('mysite.urls')),
      url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout,{'template_name': '../templates/registration/logout.html'}, name='logout'),
 ]
