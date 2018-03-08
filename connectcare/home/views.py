1796f92a38228aa2d53212be2a1addd8e5303ca6
123tarunanand <anandtarun2@gmail.com>
2018-03-08 11:08:45 +0530

    Diff for diff users

--- connectcare/home/views.py
+++ connectcare/home/views.py
@@ -4,7 +4,17 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from profiledet.models import USERMODEL
 
 @login_required()
 def main(request):
-    return render(request,'home/main.html')
+    p = USERMODEL.objects.filter(name = request.user.username)
+    if not p:
+        return render(request,'home/Gen.html')
+    k = USERMODEL.objects.get(name = request.user.username)
+    if k.type == 'Public':
+        return render(request,'home/Gen.html')
+    if k.type =='Patient':
+        return render(request,'home/PAt.html')
+    if k.type == 'Doctor':
+        return render(request,'home/Doc.html')
--- connectcare/uploads/views.py
+++ connectcare/uploads/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
-
+from profiledet.models import USERMODEL
 from .models import Document
 from .forms import DocumentForm
-
+from django.http import HttpResponseRedirect
 @login_required()
 def home(request):
-    documents = Document.objects.filter(user = request.user.username)
-    return render(request, 'uploads/home.html', { 'documents': documents })
+    p = USERMODEL.objects.filter(name = request.user.username)
+    if not p:
+        return HttpResponseRedirect("/home")
+    k = USERMODEL.objects.get(name = request.user.username)
+    if k.type == 'Patient':
+        documents = Document.objects.filter(user = request.user.username)
+        return render(request, 'uploads/home.html', { 'documents': documents })
+    else:
+        return HttpResponseRedirect("/home")
+
 
 @login_required()
 def upl(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        a = Document()
-        a.document.name = filename
-        a.user = request.user.username
-        a.description = filename
-        a.save()
-        return render(request, 'uploads/simple_upload.html', {
+    p = USERMODEL.objects.filter(name = request.user.username)
+    if not p:
+        return HttpResponseRedirect("/home")
+    k = USERMODEL.objects.get(name = request.user.username)
+    if k.type == 'Patient':
+        if request.method == 'POST' and request.FILES['myfile']:
+            myfile = request.FILES['myfile']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(filename)
+            a = Document()
+            a.document.name = filename
+            a.user = request.user.username
+            a.description = filename
+            a.save()
+            return render(request, 'uploads/simple_upload.html', {
             'uploaded_file_url': uploaded_file_url,'name':filename
-        })
-    return render(request, 'uploads/simple_upload.html')
+            })
+        return render(request, 'uploads/simple_upload.html')
+    else :
+        return HttpResponseRedirect("/home")
