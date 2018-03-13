6d232fca41e751ab28210503498da0dc7049a14a
SanjanaKrishnam <sanjana.krishnam@gmail.com>
2018-03-14 02:13:44 +0530

    something

--- connectcare/mysite/static/index.js
+++ connectcare/mysite/static/index.js
@@ -1,6 +1,13 @@
-$("#login-button").click(function(event){
-		 event.preventDefault();
-
-	 $('form').fadeOut(500);
-	 $('.wrapper').addClass('form-success');
-});
+$(function() {
+  $('<img/>').attr('src', 'http://lorempixel.com/1400/900/nature/3').load(function() {
+    $('.bg-img').append($(this));
+    // simulate loading
+    setTimeout(function() {
+     $('.container').addClass('loaded');
+    }, 1500)
+   //$(this).remove(); // prevent memory leaks as @benweet suggested
+  });
+  $('.form-toggle').on('click', function() {
+    $('.container').toggleClass('show-register')
+  })
+})
