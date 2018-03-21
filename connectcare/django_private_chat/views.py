5bf98c5961ec508feab44a93478b0969811079ba
123tarunanand <anandtarun2@gmail.com>
2018-03-21 16:51:17 +0530

    chat nearly done

--- connectcare/django_private_chat/views.py
+++ connectcare/django_private_chat/views.py
@@ -1,8 +1,12 @@
 from django.views import generic
 from braces.views import LoginRequiredMixin
 from profiledet.models import USERMODEL
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from profiledet.models import USERMODEL
+import json
+from django.http import Http404
 try:
     from django.urls import reverse
 except ImportError:
@@ -22,14 +26,24 @@ class DialogListView(LoginRequiredMixin, generic.ListView):
     ordering = 'modified'
 
     def get_queryset(self):
+        p = USERMODEL.objects.get(name = self.request.user.username)
+        if p.type=='Public':
+            raise Http404
         dialogs = models.Dialog.objects.filter(Q(owner=self.request.user) | Q(opponent=self.request.user))
         return dialogs
 
     def get_context_data(self, **kwargs):
+        p = USERMODEL.objects.get(name = self.request.user.username)
+        if p.type=='Public':
+            raise Http404
         context = super().get_context_data()
+        jd = json.decoder.JSONDecoder()
+        k = jd.decode(p.auth)
         if self.kwargs.get('username'):
             # TODO: show alert that user is not found instead of 404
             user = get_object_or_404(get_user_model(), username=self.kwargs.get('username'))
+            if self.kwargs.get('username') == self.request.user.username or self.kwargs.get('username') not in k :
+                raise Http404
             dialog = utils.get_dialogs_with_user(self.request.user, user)
             if len(dialog) == 0:
                 dialog = models.Dialog.objects.create(owner=self.request.user, opponent=user)
