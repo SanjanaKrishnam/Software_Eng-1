ef5058a65aa501d4c1e2703eeb407a857b91fa88
123tarunanand <anandtarun2@gmail.com>
2018-03-22 22:29:14 +0530

    Chatbar

--- connectcare/django_private_chat/views.py
+++ connectcare/django_private_chat/views.py
@@ -56,9 +56,8 @@ class DialogListView(LoginRequiredMixin, generic.ListView):
                 dialog = dialog[0]
             context['active_dialog'] = dialog
         else:
-            if not self.object_list:
-                return context
-        context['active_dialog'] = self.object_list[0]
+            return context
+    
         if self.request.user == context['active_dialog'].owner:
             context['opponent_username'] = context['active_dialog'].opponent.username
         else:
